d26717bfa3413db00960cc805b316aff67b5d401
Karitham <kar@karitham.dev>
2024-07-28 17:10:14 +0000

    automatically add decryption & substitute vars in kustomize

--- clusters/riko/files.sh
+++ clusters/riko/files.sh
@@ -1,5 +1,28 @@
-echo "resources:" >$(dirname "$0")/kustomization.yaml
-echo "- flux-system" >>$(dirname "$0")/kustomization.yaml
+cat <<EOF >$(dirname "$0")/kustomization.yaml
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+patches:
+  - patch: |-
+      apiVersion: kustomize.toolkit.fluxcd.io/v1
+      kind: Kustomization
+      metadata:
+        name: patch-kustomization
+      spec:
+        decryption:
+          provider: sops
+          secretRef:
+            name: sops-age
+        postBuild:
+          substitute:
+            TSNET: dolly-ruffe.ts.net
+    target:
+      kind: Kustomization
+      group: kustomize.toolkit.fluxcd.io
+      version: v1
+resources:
+  - flux-system
+EOF
+
 for file in $(find "$(dirname "$0")/apps" -name '*.yaml'); do
-    echo "- $(realpath --relative-to="$(dirname "$0")" "$file")" >>$(dirname "$0")/kustomization.yaml
+  echo "  - $(realpath --relative-to="$(dirname "$0")" "$file")" >>$(dirname "$0")/kustomization.yaml
 done
